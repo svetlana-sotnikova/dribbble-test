c655f5943adf5f702c752584a935c9a11144baf5
Svetlana Sotnikova <svetlana.data@gmail.com>
2022-07-08 17:18:13 +0300

    change cypress/e2e/spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,13 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('My First Test', () => {
+  it('Gets, types and asserts', () => {
+    cy.visit('https://example.cypress.io');
+
+    cy.contains('type').click();
+
+    cy.url().should('include', '/commands/actions');
+
+    cy.get('.action-email')
+      .type('fake@email.com')
+      .should('have.value', 'fake@email.com');
+  });
+});
